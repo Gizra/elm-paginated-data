5a012fe80d09e7321915a188250e399fe40bba13
Ryan Rempel <rgrempel@gmail.com>
2018-12-04 15:52:17 -0600

    Write a test.

--- tests/Tests.elm
+++ tests/Tests.elm
@@ -1,11 +1,19 @@
-module Tests exposing (all)
+module Tests exposing (testGetAll)
 
+import EveryDictList
 import Expect
 import Fuzz exposing (..)
+import PaginatedData exposing (..)
 import Test exposing (..)
 
 
-all : Test
-all =
-    describe "PaginatedData"
-        []
+testGetAll : Test
+testGetAll =
+    describe "getAll"
+        [ test "empty comtainers should be empty" <|
+            \_ ->
+                emptyContainer "id"
+                    |> getAll "id"
+                    |> EveryDictList.size
+                    |> Expect.equal 0
+        ]
